620e971f12a245cbdbf554de90db69273595149f
Luckford <72327770+Luckford@users.noreply.github.com>
2022-03-21 13:56:58 -0700

    Estructura del Bot

--- configs.js
+++ configs.js
@@ -1,7 +1,7 @@
 const configs = {
-    token: "OTU0NzQ5Njg2ODkwMjU0MzU2.YjXp7w.FsCA8TpguR4P5w9fOczfndLljt4",
+    token: env.TOKEN,
     prefix: "s!",
-    dbLink: "mongodb+srv://Bot:6NQDITNXJCbW3nIT@cluster0.qtoot.mongodb.net/General?retryWrites=true&w=majority"
+    dbLink: env.DBLINK
 };
 
 module.exports = {
